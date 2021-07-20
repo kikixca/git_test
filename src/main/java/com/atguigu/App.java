6702ecdff55de84667f02be62e902786f8c0cefb
xu <xu@lxy.com>
2021-07-21 00:12:09 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/App.java
+++ src/main/java/com/atguigu/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "进行测试" );
         System.out.println("还是得新建工程才爽");
         System.out.println("修改过热修复");
+        System.out.println("修改hot-fix体现merge冲突");
     }
 }
