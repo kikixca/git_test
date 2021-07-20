723d4203e07a2d9584a058144f19574082d0e6ad
xu <xu@lxy.com>
2021-07-20 23:39:10 +0800

    second commit

--- src/main/java/com/atguigu/App.java
+++ src/main/java/com/atguigu/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "进行测试" );
+        System.out.println("还是得新建工程才爽");
     }
 }
